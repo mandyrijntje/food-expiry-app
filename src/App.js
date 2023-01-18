f18450c10f6f0f2b1be6ffc5aa17c8fdfbf003e9
Bubu <msmandym@protonmail.com>
2023-01-19 00:23:48 +0100

    Created basic app components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import Banner from './Banner.js';
+import FilterBar from './FilterBar.js';
+import Footer from './Footer.js';
+import Navbar from './Navbar.js';
+import PostList from './PostList.js';
+import RentalList from './RentalList.js';
+import ReviewList from './ReviewList.js';
+
+
 
 function App() {
   return (
     <main>
-      Food expiry app
+      <Navbar />
+      <FilterBar />
+      <Banner />
+      <RentalList />
+      <ReviewList />
+      <PostList />
+      <Footer />
     </main>
   );
 }
