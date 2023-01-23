2706f571d435223204bb8fe6b223c7d0ae936e91
Bubu <msmandym@protonmail.com>
2023-01-23 16:16:29 +0100

    Edited filter bar

--- src/FilterBar.js
+++ src/FilterBar.js
@@ -3,14 +3,14 @@ import Filter from './Filter';
 // import { FaAppleAlt, FaFish, FaBreadSlice } from 'react-icons/fa';
 // import { GiBananaPeeled, GiGrapes, GiOrangeSlice, GiTomato, GiPotato, GiRoastChicken, GiMeat } from 'react-icons/gi';
 
-import { GiBananaPeeled, GiGrapes, GiOrangeSlice, GiTomato, GiRoastChicken, GiMeat } from 'react-icons/gi';
+import { GiBananaPeeled, GiOrangeSlice, GiTomato, GiRoastChicken, GiMeat } from 'react-icons/gi';
 
 export default function FilterBar() {
 
   const sorting = [
  //     {title: "Apple", icon: <FaAppleAlt />},
       {title: "Banana", icon: <GiBananaPeeled />},
-      {title: "Grape", icon: <GiGrapes />},
+      // {title: "Grape", icon: <GiGrapes />},
       {title: "Orange", icon: <GiOrangeSlice />},
       {title: "Tomato", icon: <GiTomato />},
    //   {title: "Potato", icon: <GiPotato />},
