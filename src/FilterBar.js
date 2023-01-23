639cb32e1e17b545565bded09c41d612c0316351
Bubu <msmandym@protonmail.com>
2023-01-23 17:47:36 +0100

    Refined filter categories

--- src/FilterBar.js
+++ src/FilterBar.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Filter from './Filter';
 // import { FaAppleAlt, FaFish, FaBreadSlice } from 'react-icons/fa';
-import { FaAppleAlt, FaFish } from 'react-icons/fa';
+import { FaAppleAlt, FaBreadSlice, FaFish } from 'react-icons/fa';
 // import { GiBananaPeeled, GiGrapes, GiOrangeSlice, GiTomato, GiPotato, GiRoastChicken, GiMeat } from 'react-icons/gi';
 
-import { GiGarlic, GiRoastChicken, GiMeat } from 'react-icons/gi';
+import { GiGarlic, GiRoastChicken } from 'react-icons/gi';
 
 export default function FilterBar() {
 
@@ -13,16 +13,16 @@ export default function FilterBar() {
 //  {title: "Fruits", icon: <GiBananaPeeled />},
       // {title: "Grape", icon: <GiGrapes />},
       {title: "Vegetables", icon: <GiGarlic />},
+      {title: "Carbs", icon: <FaBreadSlice />},
       // {title: "Tomato", icon: <GiTomato />},
    //   {title: "Potato", icon: <GiPotato />},
       {title: "Seafood", icon: <FaFish />},
      // {title: "Fish", icon: <FaFish />},
-      {title: "Red meat", icon: <GiMeat />},
-      {title: "White meat", icon: <GiRoastChicken />}
+      {title: "Meat", icon: <GiRoastChicken />}
     ]
   
   return (
-      <div className='flex justify-center gap-3 mt-2 border-b h-14'>
+      <div className='flex justify-start gap-3 mt-2 border-b h-14'>
         {sorting.map(item => (<Filter title={item.title} icon={item.icon}/>))}
       </div>
   )
