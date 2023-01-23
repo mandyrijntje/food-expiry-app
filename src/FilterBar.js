aefc92878aa0e7876ec58076ba464ea31d2a3c88
Bubu <msmandym@protonmail.com>
2023-01-23 17:45:22 +0100

    Refined filter categories

--- src/FilterBar.js
+++ src/FilterBar.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import Filter from './Filter';
 // import { FaAppleAlt, FaFish, FaBreadSlice } from 'react-icons/fa';
+import { FaAppleAlt, FaFish } from 'react-icons/fa';
 // import { GiBananaPeeled, GiGrapes, GiOrangeSlice, GiTomato, GiPotato, GiRoastChicken, GiMeat } from 'react-icons/gi';
 
-import { GiBananaPeeled, GiOrangeSlice, GiTomato, GiRoastChicken, GiMeat } from 'react-icons/gi';
+import { GiGarlic, GiRoastChicken, GiMeat } from 'react-icons/gi';
 
 export default function FilterBar() {
 
   const sorting = [
- //     {title: "Apple", icon: <FaAppleAlt />},
-      {title: "Banana", icon: <GiBananaPeeled />},
+      {title: "Fruits", icon: <FaAppleAlt />},
+//  {title: "Fruits", icon: <GiBananaPeeled />},
       // {title: "Grape", icon: <GiGrapes />},
-      {title: "Orange", icon: <GiOrangeSlice />},
-      {title: "Tomato", icon: <GiTomato />},
+      {title: "Vegetables", icon: <GiGarlic />},
+      // {title: "Tomato", icon: <GiTomato />},
    //   {title: "Potato", icon: <GiPotato />},
-      {title: "Chicken", icon: <GiRoastChicken />},
+      {title: "Seafood", icon: <FaFish />},
      // {title: "Fish", icon: <FaFish />},
-      {title: "Beef", icon: <GiMeat />},
-    //  {title: "Bread", icon: <FaBreadSlice />}
+      {title: "Red meat", icon: <GiMeat />},
+      {title: "White meat", icon: <GiRoastChicken />}
     ]
   
   return (
